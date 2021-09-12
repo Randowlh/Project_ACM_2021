5658c325241c04734c2c30c518ad625e9ca826ad
randowlh <1303775690@qq.com>
2021-09-12 09:54:09 +0800

    back up

--- code/tmp/1.cpp
+++ code/tmp/1.cpp
@@ -13,6 +13,89 @@ typedef double db;
 typedef long double ld;
 template<class T>inline void MAX(T &x,T y){if(y>x)x=y;}
 template<class T>inline void MIN(T &x,T y){if(y<x)x=y;}
+class DC
+{
+public:
+	struct Edge
+	{
+		int from,to,flow,cap;
+		Edge(int fr, int t, int f, int c) : from(fr), to(t), flow(f), cap(c) {}
+	};
+	bool book[maxn];
+	int cur[maxn],depth[maxn],n, t, s;
+	vector<Edge> edge;
+	vector<int> mp[maxn];
+	int bfs(int s, int t)
+	{
+		for(int i=0;i<=n;i++)
+			book[i]=0;
+		queue<int> q;
+		q.push(s);
+		book[s] = 1;
+		depth[s] = 0;
+		while (!q.empty())
+		{
+			int now = q.front();
+			q.pop();
+			for (int i = 0; i < mp[now].size(); i++)
+			{
+				Edge &e = edge[mp[now][i]];
+				if (!book[e.to] && e.cap > e.flow)
+				{
+					depth[e.to] = depth[e.from] + 1;
+					book[e.to] = 1;
+					q.push(e.to);
+				}
+			}
+		}
+		return book[t];
+	}
+	int dfs(int now, int a)
+	{
+		if (now == t || a == 0)
+			return a;
+		int flow = 0;
+		int f;
+		for (int &i = cur[now]; i < mp[now].size(); i++)
+		{
+			Edge &e = edge[mp[now][i]];
+			if (depth[now] + 1 == depth[e.to] && (f = dfs(e.to, min(a, e.cap - e.flow))) > 0)
+			{
+				e.flow += f;
+				flow += f;
+				edge[mp[now][i] ^ 1].flow -= f;
+				a -= f;
+				if (a == 0)
+					break;
+			}
+		}
+		return flow;
+	}
+	int Dinic()
+	{
+		int ans = 0;
+		while (bfs(s, t))
+		{
+			for(int i=0;i<=n;i++)
+				cur[i]=0;
+			ans += dfs(s, 0x7FFFFFFF);
+		}
+		return ans;
+	}
+	void add_edge(int from, int to, int cap)
+	{
+		edge.push_back(Edge(from, to, 0, cap));
+		edge.push_back(Edge(to, from, 0, 0));
+		mp[from].push_back(edge.size() - 2);
+		mp[to].push_back(edge.size() - 1);
+	}
+	void init(int ss,int tt,int nn){
+		s=ss,t=tt,n=nn;
+		for(int i=0;i<=n;i++)
+			mp[i].clear();
+		edge.clear();
+	}
+}
 namespace FastIO
 {
 char buf[1 << 21], buf2[1 << 21], a[20], *p1 = buf, *p2 = buf, hh = '\n';
@@ -79,80 +162,22 @@ const int m2 = 1000001011;
 const int pr=233;
 const double eps = 1e-7;
 const int maxm= 1;
-const int maxn = 510000;
-struct segtree{
-	int seg[maxn<<2];
-	int lazy[maxn<<2];
-	inline void pushup(int x){
-		seg[x]=seg[lson(x)]+seg[rson(x)];
-	}
-	inline void pushdown(int l,int r,int x){
-		if(!lazy[x])
-		return;
-		int mid=l+r>>1;
-		seg[lson(x)]+=lazy[x]*(mid-l+1);
-		lazy[lson(x)]+=lazy[x];
-		lazy[rson(x)]+=lazy[x];
-		seg[rson(x)]+=lazy[x]*(r-mid);
-		lazy[x]=0;
-	}
-	void build(int a[],int x,int l,int r){
-		lazy[x]=0;
-		if(l==r){
-			seg[x]=a[l];
-			return;	
-		}
-		int mid=l+r>>1;
-		build(a,lson(x),l,mid);
-		build(a,rson(x),mid+1,r);
-		pushup(x);
-	}
-	void update(int x,int l,int r,int ql,int qr,int v){
-		if(l>=ql&&r<=qr){
-			seg[x]+=(r-l+1)*v;
-			lazy[x]+=v;
-			return;
-		}
-		pushdown(l,r,x);
-		int mid=(l+r)>>1;
-		if(ql<=mid)
-			update(lson(x),l,mid,ql,qr,v);
-		if(qr>mid)
-			update(rson(x),mid+1,r,ql,qr,v);
-		pushup(x);
-	}
-	int query(int x,int l,int r,int ql,int qr){
-		if(ql<=l&&qr>=r)
-			return seg[x];
-		int mid=l+r>>1,ans=0;
-		pushdown(l,r,x);
-		if(ql<=mid)
-			ans+=query(lson(x),l,mid,ql,qr);
-		if(qr>mid)
-			ans+=query(rson(x),mid+1,r,ql,qr);
-		return ans;
-	}
-}tree;
-int date[maxn];
+const int maxn = 11000;
+int mp[maxn];
 void work()
 {
 	int n,m;
-	read(n,m);
-	for(int i=1;i<=n;i++)
-		read(date[i]);
-	tree.build(date,1,1,n);
+	cin>>n>>m;
+	int a,b;
 	for(int i=1;i<=m;i++){
-		int opt,a,b,c;
-		read(opt);
-		if(opt^1){
-			read(a,b);
-			print(tree.query(1,1,n,a,b));
-		}else {
-			read(a,b,c);
-			tree.update(1,1,n,a,b,c);
-		}
+		cin>>a>>b;
+		mp[a]++;
+		mp[b]++;
 	}
-	flush();
+	int ans=llinf;
+	for(int i=1;i<=n;i++)
+		MIN(ans,mp[i]);
+	cout<<ans+1<<endl;
 }
 signed main()
 {
