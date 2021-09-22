4a69db18b27b1f0155be34ed3bf76624365978fd
Rand0w <30279273+Randowlh@users.noreply.github.com>
2021-09-22 14:24:02 +0800

    back up

--- code/tmp/1.cpp
+++ code/tmp/1.cpp
@@ -13,89 +13,6 @@ typedef double db;
 typedef long double ld;
 template<class T>inline void MAX(T &x,T y){if(y>x)x=y;}
 template<class T>inline void MIN(T &x,T y){if(y<x)x=y;}
-class DC
-{
-public:
-	struct Edge
-	{
-		int from,to,flow,cap;
-		Edge(int fr, int t, int f, int c) : from(fr), to(t), flow(f), cap(c) {}
-	};
-	bool book[maxn];
-	int cur[maxn],depth[maxn],n, t, s;
-	vector<Edge> edge;
-	vector<int> mp[maxn];
-	int bfs(int s, int t)
-	{
-		for(int i=0;i<=n;i++)
-			book[i]=0;
-		queue<int> q;
-		q.push(s);
-		book[s] = 1;
-		depth[s] = 0;
-		while (!q.empty())
-		{
-			int now = q.front();
-			q.pop();
-			for (int i = 0; i < mp[now].size(); i++)
-			{
-				Edge &e = edge[mp[now][i]];
-				if (!book[e.to] && e.cap > e.flow)
-				{
-					depth[e.to] = depth[e.from] + 1;
-					book[e.to] = 1;
-					q.push(e.to);
-				}
-			}
-		}
-		return book[t];
-	}
-	int dfs(int now, int a)
-	{
-		if (now == t || a == 0)
-			return a;
-		int flow = 0;
-		int f;
-		for (int &i = cur[now]; i < mp[now].size(); i++)
-		{
-			Edge &e = edge[mp[now][i]];
-			if (depth[now] + 1 == depth[e.to] && (f = dfs(e.to, min(a, e.cap - e.flow))) > 0)
-			{
-				e.flow += f;
-				flow += f;
-				edge[mp[now][i] ^ 1].flow -= f;
-				a -= f;
-				if (a == 0)
-					break;
-			}
-		}
-		return flow;
-	}
-	int Dinic()
-	{
-		int ans = 0;
-		while (bfs(s, t))
-		{
-			for(int i=0;i<=n;i++)
-				cur[i]=0;
-			ans += dfs(s, 0x7FFFFFFF);
-		}
-		return ans;
-	}
-	void add_edge(int from, int to, int cap)
-	{
-		edge.push_back(Edge(from, to, 0, cap));
-		edge.push_back(Edge(to, from, 0, 0));
-		mp[from].push_back(edge.size() - 2);
-		mp[to].push_back(edge.size() - 1);
-	}
-	void init(int ss,int tt,int nn){
-		s=ss,t=tt,n=nn;
-		for(int i=0;i<=n;i++)
-			mp[i].clear();
-		edge.clear();
-	}
-}
 namespace FastIO
 {
 char buf[1 << 21], buf2[1 << 21], a[20], *p1 = buf, *p2 = buf, hh = '\n';
@@ -162,22 +79,38 @@ const int m2 = 1000001011;
 const int pr=233;
 const double eps = 1e-7;
 const int maxm= 1;
-const int maxn = 11000;
-int mp[maxn];
+const int maxn = 1010000;
+struct node{
+	int to,nxt;
+}eg[maxn];
+int head[maxn];
+int ecnt=0;
+void add(int u,int v){
+	eg[++ecnt].nxt=head[u];
+	eg[ecnt].to=v;
+	head[u]=ecnt;
+}
+void cl(int n){
+	ecnt=0;
+	for(int i=1;i<=n;i++)
+		head[i]=0;
+}
+int aa=0;
 void work()
 {
-	int n,m;
-	cin>>n>>m;
-	int a,b;
-	for(int i=1;i<=m;i++){
-		cin>>a>>b;
-		mp[a]++;
-		mp[b]++;
+	aa=0;
+	int n;
+	cin>>n;
+	cl(n);
+	for(int i=2;i<=n;i++){
+		int tmp;
+		cin>>tmp;
+		add(tmp,i);
 	}
-	int ans=llinf;
-	for(int i=1;i<=n;i++)
-		MIN(ans,mp[i]);
-	cout<<ans+1<<endl;
+	dfs(1,0);
+	if(aa)
+		cout<<"Takeru"<<endll;
+	else cout<<"Meiya"<<endll;
 }
 signed main()
 {
@@ -185,10 +118,10 @@ signed main()
    //freopen("in.txt","r",stdin);
 	//freopen("out.txt","w",stdout);
 #endif
-	//std::ios::sync_with_stdio(false);
-	//cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	int t = 1;
-	//cin>>t;
+	cin>>t;
 	for(int i=1;i<=t;i++){
 		//cout<<"Case #"<<i<<":"<<endll;
 		work();
