9b16507cde05119e801d917f1b302ceec7b67f23
Rand0w <30279273+Randowlh@users.noreply.github.com>
2021-10-05 14:26:46 +0800

    back up

--- code/competition/guoqing10_4/Aans.cpp
+++ code/competition/guoqing10_4/Aans.cpp
@@ -1,61 +1,63 @@
-#include 
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-map m;
-ll PH(ll x)
-{
-    ll res=x,a=x;
-    for(ll i=2;i*i<=x;i++)
-    {
-        if(a%i==0)
-        {
-            res=res/i*(i-1);
-            while(a%i==0) a/=i;
+const int maxn=110000;
+#define int long long
+#define ll long long 
+bool is_prime[maxn];
+int primes[maxn];
+int tail;
+int mo[maxn];
+ll powmod(ll a,ll b,ll mod) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+void euler(int n){
+    is_prime[1] = 1;
+    for(int i=2;i<=n;i++){
+        if(!is_prime[i])
+            primes[++tail]=i;
+        for(int j=1;j<=tail&&i*primes[j]<=n;j++){
+            is_prime[i* primes[j]]=1;
+            if(i%primes[j]==0)
+                break;
         }
     }
-    if(a>1) res=res/a*(a-1);
-    return res;
 }
-ll quick_pow(ll a,ll b,ll mod)
-{
-    ll ans=1;
-    while(b)
-    {
-        if(b&1) ans=(ans*a)%mod;
-        a=(a*a)%mod;
-        b>>=1;
-    }
+int calcphi(int x){
+    int ans=x;
+    for(int i=1;primes[i]*primes[i]<=x;i++)
+        if(x%primes[i]==0){
+            ans=ans/primes[i]*(primes[i]-1);
+            while(x%primes[i]==0)   x/=primes[i];
+        }
+    if(x!=1)
+        ans=ans/x*(x-1);
     return ans;
 }
-char s[100005];
-ll dfs(ll i,ll mod)
-{      
-    if(i==0) return 0;
-    else if(s[i]=='0') return (dfs(i-1,mod)+1)%mod;
-    else if(s[i]=='2') return ((3ll*quick_pow(2,dfs(i-1,m[mod])+1,mod)-3)%mod+mod)%mod;
-    else return (2*dfs(i-1,mod)+2)%mod;
-}
-int main()
-{
-    ll mo=1e9+7;
-    while(mo!=1)
-    {
-        m[mo]=PH(mo);
-        mo=m[mo];
+string tmp;
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    euler(1e5+100);
+    mo[0]=1e9+7;
+    for(int i=1;i<=1e5+5;i++)
+        mo[i]=calcphi(mo[i-1]);
+    int _;
+    cin>>_;
+    while(_--){
+        cin>>tmp;
+        int cnt=0;
+        for(int i=tmp.size();i>=0;i--)
+            if(tmp[i]=='2')
+                cnt++;
+        int ans=0;
+        for(int i=0;i<tmp.size();i++){
+            if(tmp[i]=='0')
+                ans++;
+            else if(tmp[i]=='1')
+                ans=((ans<<1)+2)%mo[cnt];
+            else{
+                cnt--;
+                ans=(3*powmod(2,ans+1,mo[cnt])%mo[cnt]-3+mo[cnt])%mo[cnt];
+            }
+        }
+        cout<<ans<<endl;
     }
-    m[1]=1;
-    int T;
-    scanf("%d",&T);
-    while(T--)
-    {
-        ll mod=1e9+7;
-        cin>>s+1;
-        int n=strlen(s+1);
-        printf("%lld\n",dfs(n,mod));
-    }
-    return 0;
 }
-/*
-1  x+2
-2  3*2^(x+1)-x
-*/
