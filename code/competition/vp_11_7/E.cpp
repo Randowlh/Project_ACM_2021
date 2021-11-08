24e1a7e0ba06dbfa85abae402a4c560567552a18
randowlh <1303775690@qq.com>
2021-11-08 11:04:09 +0800

    back up

--- code/competition/vp_11_7/E.cpp
+++ code/competition/vp_11_7/E.cpp
@@ -11,7 +11,39 @@ const int mod =1e9+7;
 
 int ci[N];
 
-
+template<class T>void prime_factor(T x,vector<T>&Q){
+	Q.clear();
+	while(x%2==0)Q.push_back(2),x/=2;
+	while(x%3==0)Q.push_back(3),x/=3;
+	for(T i=5;i*i<=x;i+=6)if(x%i==0||x%(i+2)==0){
+		while(x%i==0)Q.push_back(i),x/=i;
+		while(x%(i+2)==0)Q.push_back(i+2),x/=i+2;
+	}
+	if(x>1)Q.push_back(x);
+}
+int min_G(int n){
+	vector<int>Q;
+	prime_factor(n,Q);
+	if(n==2||n==4);
+	else if(n%2==1&&Q.size()>=1&&Q[0]==Q.back());
+	else if(n%4==2&&Q.size()>=2&&Q[1]==Q.back());
+	else return -1;
+	prime_factor(n-1,Q);
+	Q.erase(unique(Q.begin(),Q.end()),Q.end());
+	for(int i=1;i<n;i++){
+		bool flag=1;
+		for(auto j:Q){
+			int res=1,a=i,b=(n-1)/j;
+			while(b){
+				if(b&1)res=1ll*res*a%n;
+				a=1ll*a*a%n;
+				b>>=1;
+			}
+			if(res==1){flag=0;break;}
+		}
+		if(flag)return i;
+	}
+}
 bitset<N> bst,b2;
 int n;
 void cal(int d){
@@ -23,6 +55,7 @@ inline void solve(){
     rep(i,0,2*n) b2[i]=1;
     bool ok=0;
     vector<int> v;
+    int yg=min_G(n);
     rep(i,1,n-1){
         int x; cin>>x;
         x%=n;
@@ -35,13 +68,11 @@ inline void solve(){
     if(v.size()&&!ok){
         int cur=1;
         rep(i,1,n-1){
-            cur=cur*v[0]%n;
+            cur=cur*yg%n;
             ci[cur]=i;
         }
         bst[0]=1;
-        bst[1]=1;
-        bst[n]=1;
-        for(int i=1;i<v.size();i++)
+        for(int i=0;i<v.size();i++)
             cal(ci[v[i]]);
         if(bst[n-1]) ok=1;
     }
