af68f950fbbae1396497e7b48eb5c0abe07f1f36
randowlh <1303775690@qq.com>
2022-04-05 15:14:16 +0800

    back up

---
+++ code/tmp/tt.java
@@ -0,0 +1,18 @@
+package main;
+import java.util.Scanner;
+
+//打印“hwllo world”
+
+public class tt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hwllo world");
+    
+    
+    }
+
+
+
+
+
+}
