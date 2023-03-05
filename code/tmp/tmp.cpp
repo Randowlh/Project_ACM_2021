b72375fe74282c46a6fc1a6de0bc8d212f1cc5e3
randow <1303775690@qq.com>
2023-03-05 15:25:31 +0800

    back up

---
+++ code/tmp/tmp.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("in.txt","w",stdout);
+    cout<<5000<<endl;
+    for(int i=1;i<=5000;i++){
+        cout<<i<<" "<<i<<endl;
+    }
+}
